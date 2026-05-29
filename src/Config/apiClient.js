163164b42f64cd06c9f14fa98fda8b250950a797
Sanket <sanket202002jain>
2026-05-30 00:10:04 +0530

    fix for token

--- src/Config/apiClient.js
+++ src/Config/apiClient.js
@@ -5,9 +5,9 @@ import API_BASE_URL from "./api.config";
 const getToken = () => {
   try {
     const user = JSON.parse(localStorage.getItem("vmsUser"));
-    const raw = user?.token;
+    const raw = user?.data?.token;
     console.log("Raw:", raw);
-    return user?.token;
+    return user?.data?.token;
   } catch {
     return null;
   }
